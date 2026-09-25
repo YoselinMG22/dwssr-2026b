0a5e3d3c67d552588c914212f492c08e746f0018
YoselinMG22 <losartan2201@gmail.com>
2026-09-25 18:55:16 +0000

    🌟Feat: Nodemon Update users route response and enhance package.json for nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h1 style="color:pink"> lista de usuarios🌟</h1>');
 });
 
 //module.exports = router;
